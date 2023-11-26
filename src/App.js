6c28adcd655f83e4b23b97c3424567b345fe9fd3
Andy Truong <andytruongcs@gmail.com>
2023-11-26 11:42:33 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="background_image">
+      <div className='name'>
+        Andy's Tech Tutorials
+      </div>
+      <div className='button_links'>
+        <a href='https://www.youtube.com/c/andystechtutorials' target='_blank' rel="noreferrer">
+          <button>
+            GitHub
+          </button>
+        </a>
+        <a href='https://www.youtube.com/c/andystechtutorials' target='_blank' rel="noreferrer">
+          <button>
+            LinkedIn
+          </button>
+        </a>
+        <a href='https://www.youtube.com/c/andystechtutorials' target='_blank' rel="noreferrer">
+          <button>
+            Resume
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
