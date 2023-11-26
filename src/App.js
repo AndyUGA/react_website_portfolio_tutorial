699e2014c5ac1e21ca69ebab223f391d2b2635c6
Andy Truong <andytruongcs@gmail.com>
2023-11-26 12:36:56 -0500

    Remove unused files and refactored code

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@
 import './App.css';
 
 function App() {
+  const github_link = 'https://www.youtube.com/c/andystechtutorials';
+  const linkedin_link = 'https://www.youtube.com/c/andystechtutorials';
+  const resume_link = 'https://www.youtube.com/c/andystechtutorials';
   return (
     <div className="background_image">
       <div className='name'>
         Andy's Tech Tutorials
       </div>
       <div className='button_links'>
-        <a href='https://www.youtube.com/c/andystechtutorials' target='_blank' rel="noreferrer">
+        <a href={github_link} target='_blank' rel="noreferrer">
           <button>
             GitHub
           </button>
         </a>
-        <a href='https://www.youtube.com/c/andystechtutorials' target='_blank' rel="noreferrer">
+        <a href={linkedin_link} target='_blank' rel="noreferrer">
           <button>
             LinkedIn
           </button>
         </a>
-        <a href='https://www.youtube.com/c/andystechtutorials' target='_blank' rel="noreferrer">
+        <a href={resume_link} target='_blank' rel="noreferrer">
           <button>
             Resume
           </button>
